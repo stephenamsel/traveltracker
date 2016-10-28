2bedcea70fe428f52f5dc2a6977ecbaeac922eee
stephenamsel <amselstephen@gmail.com>
2016-10-27 23:35:51 -0400

    setting up git

---
+++ db/migrate/20161028032930_create_positions.rb
@@ -0,0 +1,12 @@
+class CreatePositions < ActiveRecord::Migration[5.0]
+  def change
+    create_table :positions do |t|
+      t.float :latitude
+      t.float :longitude
+      t.datetime :timestamp
+      t.reference :trip
+
+      t.timestamps
+    end
+  end
+end
